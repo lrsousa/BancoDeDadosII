cc6999cafbd6d254758509729d0d151b66997ebb
lrsousa <leonardo@lrsousa.com.br>
2016-04-15 02:34:58 -0300

    Faltando apenas a ultima trigger. A mesma está montada e funcionando fora da function. Mas a chamada de INSERT em historico apos a exclusao em emprestimo nao funciona.

--- TrabalhoGrauA/trabalho_grau_A.sql
+++ TrabalhoGrauA/trabalho_grau_A.sql
@@ -1,4 +1,65 @@
 ﻿/*
+CREATE OR REPLACE FUNCTION criar_historico() RETURNS TRIGGER AS
+$$
+BEGIN
+	INSERT INTO historico (idusuario, idlivro, datadevolucao, diasematraso)
+	SELECT * FROM(
+		SELECT em.idusuario, ex.idlivro, CURRENT_DATE AS datadevolucao, (CASE WHEN CURRENT_DATE - em.dataretirada > 14 THEN CURRENT_DATE - em.dataretirada ELSE 0 END) AS diasematraso
+		FROM emprestimo em JOIN exemplar ex
+		USING(idexemplar)
+		WHERE idusuario = OLD.idusuario AND idexemplar = OLD.idexemplar
+	) j;
+		
+	RAISE NOTICE 'FEITOOOOO';
+	RETURN OLD;
+END
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER criar_historico_on_delete_emprestimo
+	AFTER DELETE ON emprestimo
+	FOR EACH ROW
+	EXECUTE PROCEDURE criar_historico();
+*/
+DELETE FROM emprestimo WHERE idusuario = 4 AND idexemplar = 4;
+
+
+--retornar dados para inserir na tabela historico
+/*
+INSERT INTO historico (idusuario, idlivro, datadevolucao, diasematraso)
+SELECT * FROM(
+SELECT em.idusuario, ex.idlivro, CURRENT_DATE AS datadevolucao, (CASE WHEN CURRENT_DATE - em.dataretirada > 14 THEN CURRENT_DATE - em.dataretirada ELSE 0 END) AS diasematraso
+FROM emprestimo em JOIN exemplar ex
+USING(idexemplar)
+WHERE idusuario = 4 AND ex.idexemplar = 10
+) j
+*/
+
+/*
+CREATE OR REPLACE FUNCTION checar_qtd_livros_alugados() RETURNS TRIGGER AS
+$$
+DECLARE
+	qtd INTEGER;
+BEGIN
+	SELECT SUM(CASE WHEN e.idUsuario = NEW.idUsuario THEN 1 END)
+	FROM emprestimo e
+	INTO qtd;
+	RAISE NOTICE '%', qtd;
+	IF qtd > 2 THEN
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+*/
+/*
+CREATE TRIGGER checa_emprestimo_qtd_livros
+	BEFORE INSERT ON emprestimo
+	FOR EACH ROW
+	EXECUTE PROCEDURE checar_qtd_livros_alugados();
+*/
+--INSERT INTO emprestimo (idusuario, idexemplar, dataretirada) values (4,10,'2016-04-10');
+/*
 CREATE OR REPLACE FUNCTION calcDifDatas(Date, Date)
 RETURNS integer AS
 $$
